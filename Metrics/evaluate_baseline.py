4b0b841b3b2ec3a77f04accc4c3f438fc70c5059
Zubair Bashir <zubairbashir2004@gmail.com>
2025-12-31 19:47:10 +0530

    Add files via upload

---
+++ Metrics/evaluate_baseline.py
@@ -0,0 +1,25 @@
+import torch
+from typing import List, Callable
+from torch import Tensor
+from tqdm import tqdm
+from transformer_lens import HookedTransformer
+
+def evaluate_baseline(model: HookedTransformer, dataset, metrics: List[Callable[[Tensor], Tensor]]):
+    metrics_list = True
+    if not isinstance(metrics, list):
+        metrics = [metrics]
+        metrics_list = False
+
+    results = [[] for _ in metrics]
+    for sentence, corrupted in tqdm(dataset):
+        with torch.inference_mode():
+            logits = model(sentence)
+        for j, metric in enumerate(metrics):
+            r = metric(sentence,logits).cpu()
+            if len(r.size()) == 0:
+                r = r.unsqueeze(0)
+            results[j].append(r)
+    results = [torch.cat(rs) for rs in results]
+    if not metrics_list:
+        results = results[0]
+    return results
